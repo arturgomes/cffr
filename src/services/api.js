535b62715433d96204bc652cd3d1fd509d94aa8a
Artur Oliveira Gomes <>
2020-08-10 10:57:59 -0400

    trying to fix cors

--- src/services/api.js
+++ src/services/api.js
@@ -8,13 +8,13 @@ const api = axios.create({
   // baseURL: 'https://api.couponfeed.co/proxy',
   withCredentials: true,
   // crossorigin:true
-  // headers: {                  
+  headers: {                  
     // "Access-Control-Allow-Origin": "http://localhost:3001",
-    // "Access-Control-Allow-Origin": "https://couponfeed.co/",
+    "Access-Control-Allow-Origin": "https://www.couponfeed.co/",
   //   "Access-Control-Allow-Headers": "Authorization", 
   //   "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
   //   "Content-Type": "application/json;charset=UTF-8"                   
-  // },
+  },
   
   // baseURL: 'https://cors-anywhere.herokuapp.com/https://api.couponfeed.co',
 });
