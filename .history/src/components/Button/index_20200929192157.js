2fed0f97b77430c6508ee4098d57ed5ed9b31b56
Artur Oliveira Gomes <>
2020-09-29 19:22:07 -0400

    tava tudo dando errado no git

---
+++ .history/src/components/Button/index_20200929192157.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import ButtonFB from './ButtonFB';
+import ButtonGG from './ButtonGG';
+// import '../../assets/css/login.css'
+
+import './style.css'
+export default class LoginSocial extends Component {
+  componentFacebookRetailClicked = () => {
+    window.open("https://api.couponfeed.com.br/auth/facebook/retail", "_self");
+  }
+  componentGoogleRetailClicked = () => {
+    window.open("https://api.couponfeed.com.br/auth/google/retail", "_self");
+  }
+  componentFacebookClicked = () => {
+    window.open("https://api.couponfeed.com.br/auth/facebook", "_self");
+  }
+  componentGoogleClicked = () => {
+    window.open("https://api.couponfeed.com.br/auth/google", "_self");
+  }
+  render() {
+    let facebookData =
+      (<>
+        {/* <ul className="no-bullet"> */}
+        <ButtonFB 
+            click={() => { this.componentFacebookClicked() }}
+            caption={this.props.title}
+          /><ButtonGG
+          click={() => { this.componentGoogleClicked() }}
+          caption={this.props.title}
+        />
+          
+          
+        {/* </ul> */}
+      </>
+      )
+    let facebookDataRetail =
+      (<>
+      
+        {/* <ul className="no-bullet"> */}
+        <ButtonFB 
+            click={() => { this.componentFacebookRetailClicked() }}
+            caption={this.props.title}
+          /><ButtonGG
+          click={() => { this.componentGoogleRetailClicked() }}
+          caption={this.props.title}
+        />
+        
+        {/* </ul> */}
+      </>
+      )
+    return (
+      <div>
+        {this.props.retail === true ? facebookDataRetail : facebookData }
+      </div>
+    )
+  }
+}
