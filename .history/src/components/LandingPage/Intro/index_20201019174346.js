bc9e624fe9e1962cd6cd78f0da62f45058842f06
Artur Oliveira Gomes <>
2020-10-19 19:56:45 -0400

    quick commit

---
+++ .history/src/components/LandingPage/Intro/index_20201019174346.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../../../assets/css/fontawesome-all.css";
+import "../../../assets/css/fontawesome.css";
+// import "../../../assets/css/swiper.css";
+// import "../../../assets/css/magnific-popup.css";
+// import "../../../assets/css/styles.css";
+
+import feedback from "../../../assets/images/feedback.svg";
+
+export default class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <header
+          // id="header"
+          className="header"
+        >
+          <div className="header-content">
+            {/* <Parallax > */}
+
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="text-container">
+                    <h1>
+                      Ouça e <span className="turquoise">Encante</span>
+                      <br /> os Seus Clientes
+                    </h1>
+                    <p className="p-large">
+                      A CouponFeed veio para auxiliar o varejista na coleta de
+                      feedbacks das experiências de seu cliente. E juntos vamos
+                      além: estabelecemos um canal direto de comunicação com os
+                      seus clientes lhe permitindo <span>fechar o loop</span>{" "}
+                      após seus feedbacks.{" "}
+                    </p>
+                    <p className="p-large">
+                      {" "}
+                      E em troca, fidelize seu cliente com pontos que podem ser
+                      convertidos em descontos em produtos e também participa de
+                      promoções exclusivas.
+                    </p>
+
+                    <Link className="btn-solid-lg" href="/pricing">
+                      Experimente Agora
+                    </Link>
+                  </div>
+                </div>
+                <div className="iHide">
+                  <div className="col-lg-6">
+                    {/* <div className="image-container"> */}
+                    <img src={feedback} alt="alternative" />
+                    {/* </div> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* </Parallax> */}
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
---
+++ .history/src/services/api_20201019195639.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { getToken } from "./auth";
+
+const api = axios.create({
+  baseURL: "https://api.couponfeed.com.br",
+  withCredentials: true,
+});
+
+// portanto, tentei colocar aqui. Também não deu certo
+api.defaults.headers.common["Accept"] = "application/json";
+api.defaults.headers.common["Content-Type"] = "application/json";
+api.defaults.headers.common["Access-Control-Allow-Credentials"] = true;
+api.defaults.headers.common["Access-Control-Allow-Origin"] =
+  "https://www.couponfeed.com.br";
+
+api.interceptors.request.use(async (config) => {
+  const token = getToken();
+
+  //então tentei colocar aqui e também não deu certo.
+  config.headers.common["Accept"] = "application/json";
+  config.headers.common["Content-Type"] = "application/json";
+  config.headers.common["Access-Control-Allow-Credentials"] = true;
+  config.headers.common["Access-Control-Allow-Origin"] =
+    "https://www.couponfeed.com.br";
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
