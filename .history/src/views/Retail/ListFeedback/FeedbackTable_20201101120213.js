f606616996d29baf707dc4ddd8d9410ff8bb3ed2
Artur Oliveira Gomes <>
2020-11-01 12:02:19 -0400

    quick commit

--- .history/src/views/Retail/ListFeedback/FeedbackTable_20201101120213.js
+++ .history/src/views/Retail/ListFeedback/FeedbackTable_20201101120213.js
@@ -24,7 +24,7 @@ export default function FeedbackTable({ data }) {
       <DataGrid
         id="id"
         rows={data}
-        columns={columns1}
+        columns={columns}
         pageSize={10}
       // checkboxSelection
       />
