503b6611a6468ec0485b8ebb32d059268931360b
Artur Oliveira Gomes <>
2020-11-01 17:41:51 -0400

    quick commit

--- .history/src/views/Retail/ListFeedback/FeedbackTable_20201101174127.js
+++ .history/src/views/Retail/ListFeedback/FeedbackTable_20201101174127.js
@@ -48,7 +48,7 @@ export default function FeedbackTable({ data }) {
         rows={data}
         columns={columns}
         pageSize={50}
-        // rowsPerPageOptions={10, 25, 50}
+        rowsPerPageOptions={10, 25, 50}
         checkboxSelection={false} // TODO: selecting..
       // checkboxSelection
       />
